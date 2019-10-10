9c23ee17f395487654ec89eac032b5cfbd671bbe
Brittany Houtz <brittany.houtz@gmail.com>
2019-10-09 17:49:14 -0700

    start lab 08

--- products/products.js
+++ products/products.js
@@ -1,10 +1,10 @@
-import items from './items.js';
+import items from '..api.js';
 import renderItems from './render-items.js';
 
 const list = document.getElementById('items');
 
 for (let i = 0; i < items.length; i++) {
     const item = items[i];
-    const dom = renderItems(item);
-    list.appendChild(dom);
+    const itemElement = renderItems(item);
+    list.appendChild(itemElement);
 }
--- products/render-items.js
+++ products/render-items.js
@@ -1,3 +1,52 @@
+const emptyCart = [{ id: 'pokeball', quantity: 1 }];
+export const CART_KEY = 'cart';
+
+export const getById = (id, items) => {
+    let matchingItem;
+
+    items.forEach(item => {
+        if (item.id === id) {
+            matchingItem = item;
+        }
+    });
+
+    return matchingItem;
+};
+
+export const increaseCartItemsById = (id, cart) => {
+    let foundAMatch = false;
+    cart.forEach(order => {
+        if (order.id === id) {
+            order.quantity++;
+            foundAMatch = true;
+        }
+    });
+
+    if (foundAMatch) {
+        return;
+    } else {
+        const newItem = {
+            id: id,
+            quantity: 1,
+        };
+
+        cart.push(newItem);
+    }
+};
+
+const startEmptyCart = () => {
+    const serializedCart = JSON.stringify(emptyCart);
+
+    localStorage.setItem('cart', serializedCart);
+};
+
+const getCart = () => JSON.parse(localStorage.getItem(CART_KEY));
+
+const setCart = (storedCart) => {
+    const serializedNewCart = JSON.stringify(storedCart);
+    localStorage.setItem(CART_KEY, serializedNewCart);
+};
+
 function renderItems(item) {
     const li = document.createElement('li');
     li.classname = item.category;
@@ -19,8 +68,21 @@ function renderItems(item) {
     p.textContent = jpy;
 
     const button = document.createElement('button');
+
     button.textContent = 'Buy';
-    button.value = item.code;
+    button.value = item.id;
+    button.addEventListener('click', () => {
+        let storedCart = getCart();
+        if (!storedCart) {
+            startEmptyCart();
+            storedCart = getCart();
+        }
+    
+        let itemIncrease = getById(item.id, storedCart);
+        itemIncrease && itemIncrease.quantity++;
+    
+        setCart(storedCart);
+    });
     p.appendChild(button);
 
     li.appendChild(p);
